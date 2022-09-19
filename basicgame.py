787e2946a11708a7262142e2dcb59f389028c003
fentoncreeper <thomas000@me.com>
2022-09-19 19:13:57 -0400

    split up the drawing part and the game part

--- basicgame.py
+++ basicgame.py
@@ -18,13 +18,10 @@ def game1(pf_rows, pf_columns):
     player = 0
     # Set up the drawing window
 
-    screen = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
-
 
     # Run until the user asks to quit
 
     running = True
-    screen.fill(BLUE)
 
     while running:
 
@@ -45,8 +42,7 @@ def game1(pf_rows, pf_columns):
 
         # pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
         if pf_update == True:
-            pf_draw(pf_data, circle_radius, screen)
-            pygame.display.flip()
+            pf_draw(pf_data, circle_radius)
             pf_update = False
 
         player_display = player + 1
@@ -161,7 +157,12 @@ def check_win(pf_data, player):
 
 
 
-def pf_draw(pf_data, circle_radius, screen):
+def pf_draw(pf_data, circle_radius):
+    pygame.init()
+    # set up play field
+
+    screen = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
+    screen.fill(BLUE)
     num_rows, num_cols = pf_data.shape
     block_size_x = WINDOW_WIDTH / (num_cols + 1)
     block_size_y = WINDOW_HEIGHT / (num_rows + 1)
@@ -174,5 +175,4 @@ def pf_draw(pf_data, circle_radius, screen):
                 pygame.draw.circle(screen, (YELLOW), (block_size_x * (x + 1), block_size_y * (y + 1)), circle_radius)
             else:
                 pygame.draw.circle(screen, (WHITE), (block_size_x * (x+1), block_size_y * (y+1)), circle_radius)
-
-    return screen
+    pygame.display.flip()
